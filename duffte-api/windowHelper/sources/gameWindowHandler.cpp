2fa78b02e2161412fecc52b0bdc185234dc8aae8
Karl <typnilsengmail.com>
2020-10-31 20:37:10 +0100

    Window now draw while being resizeed

--- duffte-api/windowHelper/sources/gameWindowHandler.cpp
+++ duffte-api/windowHelper/sources/gameWindowHandler.cpp
@@ -1,4 +1,14 @@
 #include "gameWindowHandler.h"
+#include <iostream>
+
+gameWindow::gameWindow()
+{
+    glfwSetWindowUserPointer(windowOBJ.ID(), reinterpret_cast<void *>(this));
+
+    functionID = []() {
+        std::cout << "Alternative\n";
+    };
+}
 
 gameWindow::gameWindow(argPTR arg, int width, int height, std::string name)
 {
@@ -6,7 +16,8 @@ gameWindow::gameWindow(argPTR arg, int width, int height, std::string name)
     windowOBJ.setDimensions(width, height);
     windowOBJ.createWindow(name);
     windowOBJ.makeContextCurrent();
-    glfwSetFramebufferSizeCallback(windowOBJ.ID(), resizeCall);
+    glfwSetWindowUserPointer(windowOBJ.ID(), reinterpret_cast<void *>(this));
+    glfwSetWindowSizeCallback(windowOBJ.ID(), staticResizeCall);
 }
 
 gameWindow::~gameWindow()
@@ -17,14 +28,41 @@ bool gameWindow::startRenderLoop()
 {
     while (windowOBJ.runs())
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        functionID();
-        windowOBJ.swapBuffers();
-        windowOBJ.pollEvents();
+        renderContainer();
     }
     return true;
 }
-void gameWindow::resizeCall(GLFWwindow* window, int width, int height) 
+
+void gameWindow::staticResizeCall(GLFWwindow *window, int width, int height)
 {
+    gameWindow *gw = reinterpret_cast<gameWindow *>(glfwGetWindowUserPointer(window));
+    //gw->windowOBJ.setDimensions(width, height);
+    if (gw)
+        gw->resizeCall(width, height);
+    else
+    {
+        std::cout << "gw == false" << std::endl;
+    }
+}
+
+void gameWindow::resizeCall(int width, int height)
+{
+
+    renderContainer();
     glViewport(0, 0, width, height);
+}
+
+void gameWindow::renderContainer()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    functionID();
+    windowOBJ.swapBuffers();
+    windowOBJ.pollEvents();
+}
+
+
+void gameWindow::operator=(const gameWindow &gw) 
+{  
+    this->functionID = gw.functionID;
+    this->windowOBJ = gw.windowOBJ;
 }
--- duffte-api/windowHelper/sources/gameWindowHandler.h
+++ duffte-api/windowHelper/sources/gameWindowHandler.h
@@ -6,21 +6,29 @@
 class gameWindow
 {
 private:
-    Window windowOBJ;
 
     typedef void (*argPTR)();
     argPTR functionID;
 
 private:
-    static void resizeCall(GLFWwindow* window, int width, int height);
+    //Creates the resize call
+    static void staticResizeCall(GLFWwindow *window, int width, int height);
+    void resizeCall(int width, int height);
+
+    //Calls the rendering function
+    void renderContainer();
 
 public:
+    Window windowOBJ;
     //Stores a reference to a function that will be called in "startRendererLoop"
+    gameWindow();
     gameWindow(argPTR arg, int width, int height, std::string);
     ~gameWindow();
 
     //Calls the function, given by the constructor every 1/60 sec / every frame
     bool startRenderLoop();
+
+    void operator=(const gameWindow &gw);
 };
 
 #endif // __GAMEWINDOWHANDLER_H__
